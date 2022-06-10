0548206fb71c839227b9e0aa8ea3e2f5e5376c4d
Biscuit <rjfarruggia@csus.edu>
2022-06-10 16:22:35 -0700

    add aux pages, javascript for btns

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     var modeSwitch = document.querySelector('.mode-switch');
-
+    
     modeSwitch.addEventListener('click', function() {
         document.documentElement.classList.toggle('dark');
         modeSwitch.classList.toggle('active');
     });
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var today  = new Date();
+    var insertdate = document.getElementById("current-date");
+    insertdate.innerHTML = today.toLocaleDateString("en-US", options);
+
+});
+const sidebarclick = document.querySelectorAll(".app-sidebar-link");
+sidebarclick.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        sidebarclick.forEach(f => f.classList.remove("active"));
+        e.target.classList.toggle("active");
+    });
+});
     /* disable grid/list view for now, -R
         var listView = document.querySelector('.list-view');
         var gridView = document.querySelector('.grid-view');
@@ -23,4 +36,3 @@ document.addEventListener('DOMContentLoaded', function() {
             projectsList.classList.remove('jsListView');
             projectsList.classList.add('jsGridView');
         });*/
-});
