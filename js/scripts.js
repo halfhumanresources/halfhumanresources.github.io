66aec19d79c3d11ca8ee648abec616d55dce92c9
Biscuit <rjfarruggia@csus.edu>
2022-06-11 03:21:55 -0700

    persistent dark mode script

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
+    /*
     var modeSwitch = document.querySelector('.mode-switch');
     
     modeSwitch.addEventListener('click', function() {
         document.documentElement.classList.toggle('dark');
         modeSwitch.classList.toggle('active');
-    });
+    });*/
+
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     var today  = new Date();
     var insertdate = document.getElementById("current-date");
     insertdate.innerHTML = today.toLocaleDateString("en-US", options);
 
+    const darkswitch = document.getElementById('darkbutton');
+
+    darkswitch.addEventListener('click', () => {
+        document.documentElement.classList.toggle('dark');
+        if (document.documentElement.classList.contains('dark')) {
+            localStorage.setItem('darkMode', 'enabled');
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+        }
+    });
+    if (localStorage.getItem('darkMode') == 'enabled') {
+        document.documentElement.classList.toggle('dark');
+    }
 });
+
+
+
+
+
 const sidebarclick = document.querySelectorAll(".app-sidebar-link");
 sidebarclick.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -18,16 +38,7 @@ sidebarclick.forEach((btn) => {
         e.target.classList.toggle("active");
     });
 });
-const moonicon = document.querySelector(".moon-icon");
-moonicon.addEventListener('click', function() {
-    if (moonicon.classList.contains('fa-moon')) {
-        moonicon.classList.remove('fa-moon');
-        moonicon.classList.add('fa-lightbulb');
-    } else {
-        moonicon.classList.add('fa-moon')
-        moonicon.classList.remove('fa-lightbulb');
-    }
-});
+
     /* disable grid/list view for now, -R
         var listView = document.querySelector('.list-view');
         var gridView = document.querySelector('.grid-view');
