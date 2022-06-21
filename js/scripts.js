f7abd63b417517df3450501217acf6fb7fc62f08
Biscuit <rjfarruggia@csus.edu>
2022-06-20 18:15:55 -0700

    cleanup javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    /*
-    var modeSwitch = document.querySelector('.mode-switch');
-    
-    modeSwitch.addEventListener('click', function() {
-        document.documentElement.classList.toggle('dark');
-        modeSwitch.classList.toggle('active');
-    });*/
-    /* $('.app-container').fadeIn(300);*/
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     var today  = new Date();
     var insertdate = document.getElementById("current-date");
@@ -27,10 +19,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-
-
-
-
 const sidebarclick = document.querySelectorAll(".app-sidebar-link");
 sidebarclick.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -38,22 +26,3 @@ sidebarclick.forEach((btn) => {
         e.target.classList.toggle("active");
     });
 });
-
-    /* disable grid/list view for now, -R
-        var listView = document.querySelector('.list-view');
-        var gridView = document.querySelector('.grid-view');
-        var projectsList = document.querySelector('.project-boxes');
-
-        listView.addEventListener('click', function() {
-            gridView.classList.remove('active');
-            listView.classList.add('active');
-            projectsList.classList.remove('jsGridView');
-            projectsList.classList.add('jsListView');
-        });
-
-        gridView.addEventListener('click', function() {
-            gridView.classList.add('active');
-            listView.classList.remove('active');
-            projectsList.classList.remove('jsListView');
-            projectsList.classList.add('jsGridView');
-        });*/
