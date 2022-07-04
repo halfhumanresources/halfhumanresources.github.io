6ecf7737ffda9eae855eb6997e0ca0d846b38815
thuang0 <89866629+thuang0@users.noreply.github.com>
2022-07-04 12:30:49 -0700

    Update element.py

--- halfhumantest/element.py
+++ halfhumantest/element.py
@@ -5,14 +5,6 @@ class BasePageElement():
     def __set__(self, obj, value):
         driver = obj.driver
         WebDriverWait(driver, 100).until(
-            lambda driver: driver.find_element(By.XPATH, self.locator))
-        driver.find_element(By.XPATH, self.locator).clear()
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-
-class BasePageElement():
-    def __set__(self, obj, value):
-        driver = obj.driver
-        WebDriverWait(driver, 100).until(
-            lambda driver: driver.find_element(By.XPATH, self.locator))
-        driver.find_element(By.XPATH, self.locator).clear()
+            lambda driver: driver.find_element(By.ID, self.locator))
+        driver.find_element(By.ID, self.locator).clear()
+        driver.find_element(By.ID, self.locator).send_keys(value)
