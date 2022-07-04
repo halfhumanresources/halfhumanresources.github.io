1a5a9931df7e6b6c752ca4d9d32f80cafbf0c9c7
thuang0 <89866629+thuang0@users.noreply.github.com>
2022-07-04 12:31:34 -0700

    Update locators.py

--- halfhumantest/locators.py
+++ halfhumantest/locators.py
@@ -4,7 +4,7 @@ class MainPageLocators():
     
     HEALTH_BUTTON = (By.XPATH, '/html/body/div/div[3]/div[2]/div[2]/a[1]')
     DATA_BUTTON = (By.XPATH, '/html/body/div/div[3]/div[2]/div[2]/a[2]')
-    STATS_BUTTON = (By.XPATH, '/html/body/div/div[3]/div[2]/div[2]/a[3]')
+    VACATION_BUTTON = (By.XPATH, '/html/body/div/div[3]/div[2]/div[2]/a[3]')
     OTHER_BUTTON = (By.XPATH, '/html/body/div/div[3]/div[2]/div[2]/a[4]')
     VENDIA_BUTTON = (By.XPATH, '/html/body/div/div[3]/div[2]/div[2]/a[5]')
 
@@ -14,28 +14,8 @@ class HealthPageLocators():
     RR_BUTTON = (By.XPATH, '/html/body/div/div[2]/div[2]/div[2]/div/div[2]/label[3]')
 
 class DataPageLocators():
-    CREATE_EMPLOYEE_BUTTON = (By.XPATH, '/html/body/div/div[2]/div[2]/div[4]/button[1]')
-    DELETE_EMPLOYEE_BUTTON = (By.XPATH, '/html/body/div/div[2]/div[2]/div[4]/button[2]')
+    CREATE_EMPLOYEE_BUTTON = (By.CLASS_NAME, 'create-btn')
+    DELETE_EMPLOYEE_BUTTON = (By.CLASS_NAME, 'delete-btn')
+    SUBMIT_EMPLOYEE_BUTTON = (By.XPATH, '//*[@id="create-btn"]')
+    SUBMIT_ID_BUTTON = (By.XPATH, '/html/body/div/div[2]/div[2]/div[7]/div/button[1]')
     SHOW_ID_BUTTON = (By.XPATH, '//*[@id="toggleIDText"]')
-    SUBMIT_EMPLOYEE_BUTTON = (By.XPATH, '/html/body/div/div[2]/div[2]/div[5]/div[3]/button[1]')
-=======
-from selenium.webdriver.common.by import By
-
-class MainPageLocators():
-    
-    HEALTH_BUTTON = (By.XPATH, '/html/body/div/div[3]/div[2]/div[2]/a[1]')
-    DATA_BUTTON = (By.XPATH, '/html/body/div/div[3]/div[2]/div[2]/a[2]')
-    STATS_BUTTON = (By.XPATH, '/html/body/div/div[3]/div[2]/div[2]/a[3]')
-    OTHER_BUTTON = (By.XPATH, '/html/body/div/div[3]/div[2]/div[2]/a[4]')
-    VENDIA_BUTTON = (By.XPATH, '/html/body/div/div[3]/div[2]/div[2]/a[5]')
-
-class HealthPageLocators():
-    PULSE_RATE_BUTTON = (By.XPATH, '/html/body/div/div[2]/div[2]/div[2]/div/div[2]/label[1]')
-    EXERCISE_BUTTON = (By.XPATH, '/html/body/div/div[2]/div[2]/div[2]/div/div[2]/label[2]')
-    RR_BUTTON = (By.XPATH, '/html/body/div/div[2]/div[2]/div[2]/div/div[2]/label[3]')
-
-class DataPageLocators():
-    CREATE_EMPLOYEE_BUTTON = (By.XPATH, '/html/body/div/div[2]/div[2]/div[4]/button[1]')
-    DELETE_EMPLOYEE_BUTTON = (By.XPATH, '/html/body/div/div[2]/div[2]/div[4]/button[2]')
-    SHOW_ID_BUTTON = (By.XPATH, '//*[@id="toggleIDText"]')
-    SUBMIT_EMPLOYEE_BUTTON = (By.XPATH, '/html/body/div/div[2]/div[2]/div[5]/div[3]/button[1]')
